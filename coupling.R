b14429b2d4badf8c6aff5d3c2899ea63547f858f
Waschina <s.waschina@posteo.net>
2017-10-05 13:08:03 +0200

    - new function to join n metabolic models (so-called community models) - new function to add coupling constraints to community models

--- coupling.R
+++ coupling.R
@@ -29,3 +29,23 @@ add_coupling <- function(coupling, react, x, lb, ub, rtype){
   coupling$rtype[index]   <- rtype
   return(coupling)
 }
+
+get_coupling_constraints_mult <- function(modj) {
+  coupling <- list(react=list(), x=list(), lb=vector(), ub=vector(), rtype=vector())
+  intern_rea  <- grep("EX_", modj@react_id, invert = T)
+  allObj.ind  <- which(modj@obj_coef!=0)
+  allObj.name <- react_id(modj)[allObj.ind]
+  for(j in intern_rea){
+    # get corresponding objective reaction for reaction j
+    m.id <- gsub("_.*", "", react_id(modj)[j])
+    obj.ind <- grep(paste0("^",m.id,"_"),allObj.name)
+    obj <- allObj.ind[obj.ind]
+    
+    reversible <- modj@lowbnd[j] < 0
+    # add coupling backwards direction
+    if(reversible) coupling <- add_coupling(coupling, react=c(j, obj), x=c(1,400), lb=0.01, ub=NA, rtype="L")
+    # add coupling forward direction
+    coupling <- add_coupling(coupling, react=c(j, obj), x=c(1,-400), lb=NA, ub=0.01, rtype="U")
+  }
+  return(coupling)
+}
---
+++ join_mult_models.R
@@ -0,0 +1,78 @@
+#
+# Joins multiple metabolic models (imported into R with sybilSBML)
+#
+join_mult_models <- function(model.list) {
+  require(data.table)
+  n <- length(model.list)
+  
+  model.IDs <- data.table(model.name = lapply(model.list,FUN = function(x) x@mod_desc),
+                          model.id = as.character(1:n))
+  model.IDs$model.id <- paste0("M",model.IDs$model.id)
+  
+  # get a table of all exchange reactions and their lower bounds
+  cat("Extracting all metabolite exchange (i.e. \"EX\") reactions...\n")
+  ex.rxns <- data.table(model.name = character(0L), react_id = character(0L), lb = double(0L))
+  for(i in 1:n) {
+    ex.ind <- grep("^EX_",model.list[[i]]@react_id)
+    tmp <- data.table(model.name = model.list[[i]]@mod_desc,
+                      react_id = model.list[[i]]@react_id[ex.ind],
+                      lb = model.list[[i]]@lowbnd[ex.ind])
+    ex.rxns <- rbind(ex.rxns,tmp)
+  }
+  ex.rxns[,model.name := NULL]
+  setkey(ex.rxns,NULL)
+  ex.rxns <- unique(ex.rxns)
+  if(any(duplicated(ex.rxns$react_id))) 
+    stop("unequal lower bounds for at least one exchange reaction.")
+  ex.rxns[,met := gsub("^EX_","",react_id)]
+  
+  # rename reactions and metabolites
+  cat("Renaming reaction-, metabolite, and compartment IDs...\n")
+  for(i in 1:n){
+    model.list[[i]]@react_id    <- paste0(model.IDs$model.id[i],"_", model.list[[i]]@react_id)
+    model.list[[i]]@met_id      <- paste0(model.IDs$model.id[i],"_", model.list[[i]]@met_id)
+    model.list[[i]]@mod_compart <- paste0(model.IDs$model.id[i],"_", model.list[[i]]@mod_compart)
+  }
+  
+  
+  # initiating joined/community model.
+  compNames <- character(0)
+  for(i in 1:n)
+    compNames <- c(compNames,model.list[[i]]@mod_compart)
+    
+  modj <- modelorg(id = "joined.mod",
+                   name = paste0("Number of species in community: ",n),
+                   compartment = compNames)
+  
+  # add reactions from single models to joined/community model
+  cat("Adding reactions from individual species to joined/community model...\n")
+  for(i in 1:n) {
+    cat(paste0("\r",i,"/",n))
+    n.mets <- length(modj@met_comp)
+    max.c <- ifelse(n.mets>0,max(modj@met_comp),0)
+    model.list[[i]]@met_comp <- model.list[[i]]@met_comp + as.integer(max.c)
+    modj <- addMultiReact(modj, ids=model.list[[i]]@react_id, src = model.list[[i]])
+  }
+  
+  
+  # add new external exchanges
+  cat("\rAdding new external exchanges...\n")
+  mod_compart(modj) <- c(mod_compart(modj), "e")
+  Nex <- nrow(ex.rxns)
+  modj <- addMultiReact(model=modj, ids=ex.rxns$react_id, mets=ex.rxns$met, Scoefs = rep(-1, Nex), 
+                        reversible = TRUE, lb=ex.rxns$lb, metComp = rep("e", Nex))
+  mod_name(modj) <- "A SW - joined model."
+
+  # setting up original exchange interactions to interact with new common "e" compartment
+  # e.g. "M1_ac(e) <->"  ==> "M1_ac(e) <-> ac(e)" + removing lower bnd (new: -1000)
+  cat("Modifying original exchange reactions to interact with new common \"e\" compartment...\n")
+  r.ind <- grep("^M[0-9]+_EX_",modj@react_id)
+  tmp.mets <- gsub("^M[0-9]+_EX_","",modj@react_id[r.ind])
+  m.ind.e  <- match(tmp.mets, modj@met_id)
+  modj@lowbnd[r.ind] <- rep(-1000,length(r.ind))
+  for(i in 1:length(r.ind))
+    modj@S[m.ind.e[i],r.ind[i]] <- 1
+  
+  return(list(model.IDs=model.IDs,
+              modj = modj))
+}
